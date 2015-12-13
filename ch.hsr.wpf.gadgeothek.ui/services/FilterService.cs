dc7e608626d11c3ed177bf15fb2c9025075bc67b
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-12-13 10:56:02 +0100

    ADD: filter service

---
+++ ch.hsr.wpf.gadgeothek.ui/services/FilterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ch.hsr.wpf.gadgeothek.ui.viewmodel;
+
+namespace ch.hsr.wpf.gadgeothek.ui.services
+{
+    public class FilterService<T> : INotifyCollectionChanged
+    {
+
+    }
+}
